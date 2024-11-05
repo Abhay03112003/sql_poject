cb0ad91ef2cb21ad1f1d560b96bb5aaffab20537
Abhay03112003 <157887314+Abhay03112003@users.noreply.github.com>
2024-11-05 08:01:27 +0530

    Add files via upload

---
+++ project_sql.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE CollegeERP;
+
+USE CollegeERP;
+
+-- Table for Students
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(20),
+    email VARCHAR(50),
+    phone VARCHAR(15)
+);
+
+-- Table for Teachers
+CREATE TABLE teachers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(20),
+    email VARCHAR(50),
+    phone VARCHAR(15)
+);
+select * from students;
+select * from teachers;
